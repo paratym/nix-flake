30144e6e00f6f0293ddd356c1460280aea369e1c
ben <benbaldwin000@gmail.com>
2024-02-16 16:11:56 -0700

    even more secure db ;)

--- modules/nixos/postgres.nix
+++ modules/nixos/postgres.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }: {
-
   services.postgresql = {
     enable = true;
+    enableTCPIP = true;
+    ensureDatabases = [ "crud" ];
     authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
-      local all       all     trust
+      # type database user   address        auth
+      local  all      all                   trust
+      host   all      all    localhost      trust
+      host   all      all    192.168.0.0/16 trust
     '';
   };
 }
