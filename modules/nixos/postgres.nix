a21019fb20cca572a031270dcae610c895cf74bf
ben <benbaldwin000@gmail.com>
2024-02-19 14:35:17 -0700

    pg ssl

--- modules/nixos/postgres.nix
+++ modules/nixos/postgres.nix
@@ -4,6 +4,7 @@
     enable = true;
     package = pkgs.postgresql_16;
     enableTCPIP = true;
+    settings.ssl = true;
     ensureDatabases = [ "crud" ];
     authentication = pkgs.lib.mkOverride 10 ''
       # type database user   address        auth
